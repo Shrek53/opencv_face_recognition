973dcd5cf45f554fb64d88c0407f6206397759eb
shrek53 <apon.ruet@gmail.com>
2019-11-03 02:38:36 +0600

    auto file name append using existing files in dataset

--- collect_data_set/collect_training_data.py
+++ collect_data_set/collect_training_data.py
@@ -1,17 +1,23 @@
 import os, sys
+import glob
 import cv2
 
 cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("test")
-
+cv2.namedWindow("Capture Images")
 img_counter = 0
 folder_name = input("Enter person name: ")
-folder_path = './dataset/{}'.format(folder_name)
-os.mkdir(folder_path)
+folder_path = './dataset/{}/'.format(folder_name)
+if not os.path.isdir(folder_path):
+    os.mkdir(folder_path)
+else:
+    files = file_count = glob.glob(folder_path + '*.*')
+    file_count_in_folder = len(files)
+    # print('number of files - {}'.format(file_count_in_folder))
+    img_counter = file_count_in_folder
+
 while True:
     ret, frame = cam.read()
-    cv2.imshow("test", frame)
+    cv2.imshow("Capture Images", frame)
     if not ret:
         break
     k = cv2.waitKey(1)
